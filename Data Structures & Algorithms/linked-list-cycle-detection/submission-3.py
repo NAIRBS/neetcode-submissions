b7fb64a15bc0084172dc3ff90dfcf6a0430e4e1f
NAIRBS <sohhanyu@gmail.com>
2026-08-29 20:30:55 +0800

    Add: linked-list-cycle-detection - submission-3

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-3.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # hashmap = {} # Use a hashmap to store visited node locations (in memory!)
+        # currentNode = head
+        # while currentNode:
+        #     if currentNode.next not in hashmap: hashmap[currentNode.next] = 1
+        #     else: return True
+        #     currentNode = currentNode.next
+        # return False
+
+        # The classic tortoise and hare method
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast: return True
+        return False
+        
