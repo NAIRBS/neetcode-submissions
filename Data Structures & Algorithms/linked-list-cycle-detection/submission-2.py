c344722e979647067f27da4924f15bce454bffa7
NAIRBS <sohhanyu@gmail.com>
2026-08-29 20:23:56 +0800

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        hashmap = {} # Use a hashmap to store visited node locations (in memory!)
+        currentNode = head
+        while currentNode:
+            if currentNode.next not in hashmap: hashmap[currentNode.next] = 1
+            else: return True
+            currentNode = currentNode.next
+        return False
+        
